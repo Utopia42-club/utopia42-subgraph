7b62176a899429a37a879f3402302632f05a2f73
jafari-mi <mi.jafari70@gmail.com>
2022-08-16 12:47:52 +0430

    change factory address

--- verse/src/helpers.ts
+++ verse/src/helpers.ts
@@ -1,7 +1,7 @@
 import { Address } from '@graphprotocol/graph-ts'
 import { Utopia42Verse } from '../generated/templates/Utopia42Verse/Utopia42Verse'
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0x71E9d0bB75EC8320d0Af229e957e9dc28E84f4b9'
 
 export function getVerseName(verseAddress: Address): String {
     let verseContract = Utopia42Verse.bind(Address.fromString(verseAddress.toHexString()))
