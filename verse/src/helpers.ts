ddce0ff41c06c0b29a2325f62dd24884886c64d5
jafari-mi <mi.jafari70@gmail.com>
2022-08-18 15:17:36 +0430

    Swith to main-net

--- verse/src/helpers.ts
+++ verse/src/helpers.ts
@@ -1,7 +1,7 @@
 import { Address } from '@graphprotocol/graph-ts'
 import { Utopia42Verse } from '../generated/templates/Utopia42Verse/Utopia42Verse'
 
-export const FACTORY_ADDRESS = '0x71E9d0bB75EC8320d0Af229e957e9dc28E84f4b9'
+export const FACTORY_ADDRESS = '0xFcdF8f39CFe0a928081cC8C4D0eF3f6E2CE5452E'
 
 export function getVerseName(verseAddress: Address): String {
     let verseContract = Utopia42Verse.bind(Address.fromString(verseAddress.toHexString()))
