ae0289cc3af576ec56f32c388da168fe77480b95
jafari-mi <mi.jafari70@gmail.com>
2022-10-16 10:28:30 +0330

    total verified

--- citizenID/generated/schema.ts
+++ citizenID/generated/schema.ts
@@ -779,3 +779,44 @@ export class CitizenIDVerification extends Entity {
     this.set("citizen", Value.fromBytes(value));
   }
 }
+
+export class Counter extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save Counter entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        `Entities of type Counter must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+      );
+      store.set("Counter", id.toString(), this);
+    }
+  }
+
+  static load(id: string): Counter | null {
+    return changetype<Counter | null>(store.get("Counter", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get totalVerified(): BigInt {
+    let value = this.get("totalVerified");
+    return value!.toBigInt();
+  }
+
+  set totalVerified(value: BigInt) {
+    this.set("totalVerified", Value.fromBigInt(value));
+  }
+}
--- citizenID/src/mapping.ts
+++ citizenID/src/mapping.ts
@@ -1,19 +1,29 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
-    BrightIDSet
+    BrightIDSet,
 } from "../generated/citizenVerification/CitizenIDVerification"
-import { CitizenIDVerification } from "../generated/schema"
+import { CitizenIDVerification, Counter } from "../generated/schema"
+
+const citizenIdContractAddress = '0xEDa45366e9135920403A15Dc697b021f80Ff9d2D'
 
 export function handleBrightIDSet(event: BrightIDSet): void {
   let entity = CitizenIDVerification.load(event.params.nftId.toString())
 
   if (!entity) {
     entity = new CitizenIDVerification(event.params.nftId.toString())
-
   }
+
+  let counter = Counter.load(citizenIdContractAddress)
+  if (!counter) {
+    counter = new Counter(citizenIdContractAddress)
+    counter.totalVerified = BigInt.fromI32(0)
+  }
+  counter.totalVerified = counter.totalVerified.plus(BigInt.fromI32(1))
+
   entity.citizen = event.params.addrs[0]
   entity.tokenId = event.params.nftId
   entity.verifier = event.params.caller
   entity.save()
+  counter.save()
 }
 
